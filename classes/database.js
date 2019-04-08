d709419691cb3a720876d1ccbc5709b18e54902b
Alex <threebowpr@gmail.com>
2019-04-07 22:12:17 -0700

    Use knex and objection instead of sequelize

--- classes/database.js
+++ classes/database.js
@@ -1,29 +1,28 @@
-const Sequelize = require("sequelize"),
+const Knex = require("knex"),
 	  path = require("path"),
 	  fs = require("fs");
 
-class Database extends Sequelize {
-	constructor() {
-		super(...arguments);
+class Database {
+	constructor({options, models}) {
+		this.knex = new Knex(options);
+		this.loadModels(models);
 	}
 
 	loadModels(dir) {
+		this.models = {};
+
 		let files = fs.readdirSync(dir);
 		files.forEach(f => {
 			if(!f.endsWith(".js")) return;
-			this.import(path.join(dir, f));
-		});
-
-		Object.keys(this.models).forEach(name => {
-			if(this.models[name].associate) {
-				this.models[name].associate(this.models);
-			}
+			let model = require(path.join(dir, f));
+			model.knex(this.knex);
+			this.models[model.name] = model;
 		});
 	}
 }
 
 //Pagination helper
-Database.Model.paginate = async function(pageNum, max, options) {
+/*Database.Model.paginate = async function(pageNum, max, options) {
 	let page = Math.max(1, Math.floor(Number(pageNum) || 1));
 	let offset = (page-1)*max;
 
@@ -35,6 +34,6 @@ Database.Model.paginate = async function(pageNum, max, options) {
 
 	let totalPages = Math.ceil(count/max);
 	return {rows, totalPages, perPage: max, currentPage: page}
-};
+};*/
 
 module.exports = Database;
