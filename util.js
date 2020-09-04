99379995c8cbb37cbba2a709a43d0e1b63648a62
Alex <threebowpr@gmail.com>
2020-09-04 12:02:23 -0700

    Throw error in ResolveRoute for nonexistant route

--- util.js
+++ util.js
@@ -10,6 +10,7 @@ module.exports.FormatPathName = (str) => {
 module.exports.ResolveRoute = (name, params) => {
 	//Return the plain path if there are no arguments
 	let path = RouteList[name];
+	if(!path) throw new Error(`A route named '${name}' does not exist.`);
 	if(!params) return path;
 
 	//Compile the path with the given parameters if they are given
