3699f04949c933221e4d7fa92cf6b80d1c5095fd
Alex <threebowpr@gmail.com>
2020-11-16 11:00:09 -0800

    Add wrapping of async middleware to middlewareGroup

--- classes/middlewareGroup.js
+++ classes/middlewareGroup.js
@@ -4,6 +4,30 @@ module.exports = class MiddlewareGroup {
 		this.members = members;
 	}
 
+	static _wrapMiddleware(arr) {
+		let ret = [];
+
+		for(let i = 0; i < arr.length; i++) {
+			let fn = arr[i];
+
+			if(fn.length === 4) {
+				ret.push(fn);
+				continue;
+			}
+
+			ret.push(async (req, res, next) => {
+				try {
+					let promise = fn(req, res, next);
+					if(promise && promise.then && promise.catch) await promise;
+				} catch(e) {
+					next(e);
+				}
+			});
+		}
+
+		return ret;
+	}
+
 	static getStack(app, groupNames) {
 		let fns = [];
 
@@ -13,6 +37,6 @@ module.exports = class MiddlewareGroup {
 			fns.push(...group.members);
 		});
 
-		return fns;
+		return this._wrapMiddleware(fns);
 	}
 };
