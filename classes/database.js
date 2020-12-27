53fd6f76bbce8dc53cb22a4187c9a479a4a6b5b8
Alex <threebowpr@gmail.com>
2020-12-26 22:55:10 -0800

    Code cleanup

--- classes/database.js
+++ classes/database.js
@@ -1,39 +1,25 @@
 const Knex = require("knex"),
 	  path = require("path"),
-	  fs = require("fs");
+	  fs   = require("fs");
 
-class Database {
-	constructor({options, models}) {
-		this.knex = new Knex(options);
-		this.loadModels(models);
+module.exports = class Database {
+	constructor(options) {
+		this._knex = new Knex(options.knexOptions);
+		this._loadModels(options.modelPath);
 	}
 
-	loadModels(dir) {
+	_loadModels(dir) {
 		this.models = {};
 
 		let files = fs.readdirSync(dir);
-		files.forEach(f => {
-			if(!f.endsWith(".js")) return;
-			let model = require(path.join(dir, f));
-			model.knex(this.knex);
+
+		for(let i = 0; i < files.length; i++) {
+			let file = files[i];
+			if(!file.endsWith(".js")) continue;
+
+			let model = require(path.join(dir, file));
+			model.knex(this._knex);
 			this.models[model.name] = model;
-		});
+		}
 	}
-}
-
-//Pagination helper
-/*Database.Model.paginate = async function(pageNum, max, options) {
-	let page = Math.max(1, Math.floor(Number(pageNum) || 1));
-	let offset = (page-1)*max;
-
-	options = options || {};
-	options.limit = max;
-	options.offset = offset;
-
-	let {rows, count} = await this.findAndCountAll(options);
-
-	let totalPages = Math.ceil(count/max);
-	return {rows, totalPages, perPage: max, currentPage: page}
-};*/
-
-module.exports = Database;
+};
