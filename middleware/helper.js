96090c25798f910ef8902fd5b9a3e8d57e7ae8ac
Alex <threebowpr@gmail.com>
2021-01-04 19:00:33 -0800

    Add req.realIp property in HelperMiddleware

--- middleware/helper.js
+++ middleware/helper.js
@@ -1,5 +1,7 @@
 module.exports = class HelperMiddleware extends require("../classes/middleware") {
 	run(req) {
+		req.realIp = req.get("CF-Connecting-IP") ?? req.ip;
+
 		let wantsHtml = req.accepts("html") !== false;
 		let wantsJson = req.accepts("json") !== false;
 
