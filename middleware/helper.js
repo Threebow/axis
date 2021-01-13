f4920550acbb00f359b51a6833a9259a60fb26e0
Alex <threebowpr@gmail.com>
2021-01-13 13:40:12 -0800

    Add comments to helper middleware

--- middleware/helper.js
+++ middleware/helper.js
@@ -1,10 +1,11 @@
 module.exports = class HelperMiddleware extends require("../classes/middleware") {
 	run(req) {
+		//Set real IP address
 		req.realIp = req.get("CF-Connecting-IP") ?? req.ip;
 
+		//Determine response type
 		let wantsHtml = req.accepts("html") !== false;
 		let wantsJson = req.accepts("json") !== false;
-
 		req.wantsJsonResponse = wantsJson && !wantsHtml;
 	}
 };
