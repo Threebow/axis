262c0619b62b2256acab8261a4e2bf71e4d79d62
Alex <threebowpr@gmail.com>
2021-01-13 13:38:08 -0800

    Remove destroy method from controller

--- classes/controller.js
+++ classes/controller.js
@@ -1,8 +1,5 @@
 const Responder = require("./responder");
 
 module.exports = class Controller extends Responder {
-	async destroy(req, res, model) {
-		await model.$query().delete();
-		return 204;
-	}
+	/* ... */
 };
