ba3b081dc3a6cb65b6f53bfdb68dfac934565e0a
Alex <threebowpr@gmail.com>
2021-01-13 13:48:51 -0800

    Add frontend asset delivery helper function to helper middleware

--- middleware/helper.js
+++ middleware/helper.js
@@ -1,4 +1,20 @@
+const path = require("path");
+
 module.exports = class HelperMiddleware extends require("../classes/middleware") {
+	constructor(...args) {
+		super(...args);
+
+		if(this._app._expressOptions.staticContentRoot) {
+			try {
+				this._mixManifest = require(path.join(this._app._expressOptions.staticContentRoot, "./mix-manifest.json"));
+			} catch(e) {
+				if(e.code === "MODULE_NOT_FOUND") {
+					this._mixManifest = {};
+				} else throw e;
+			}
+		}
+	}
+
 	run(req, res) {
 		//Set real IP address
 		req.realIp = req.get("CF-Connecting-IP") ?? req.ip;
@@ -10,5 +26,8 @@ module.exports = class HelperMiddleware extends require("../classes/middleware")
 
 		//Store user in locals
 		res.locals.user = req.user;
+
+		//Frontend asset delivery
+		res.locals.asset = (s) => this._mixManifest[s] ?? s;
 	}
 };
