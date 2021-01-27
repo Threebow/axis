5758afdc137ea975827846f7458e75523762b96b
Alex <threebowpr@gmail.com>
2021-01-27 14:41:04 -0800

    Rework database to test for connectivity at the start, and allow injection of the IoC container into models

--- classes/database.js
+++ classes/database.js
@@ -1,25 +1,49 @@
-const Knex = require("knex"),
-	  path = require("path"),
-	  fs   = require("fs");
+const ContainerProxy = require("./containerProxy"),
+	  BaseModel      = require("./baseModel"),
+	  Knex           = require("knex"),
+	  path           = require("path"),
+	  fs             = require("fs-extra"),
+	  _              = require("lodash");
 
 module.exports = class Database {
-	constructor(options) {
-		this._knex = new Knex(options.knexOptions);
-		this._loadModels(options.modelPath);
+	constructor(app, options) {
+		this._app = app;
+		this._options = options;
 	}
 
-	_loadModels(dir) {
-		this.models = {};
+	async initialize() {
+		//Instantiate knex
+		this._knex = new Knex(this._options.knexOptions);
 
-		let files = fs.readdirSync(dir);
-
-		for(let i = 0; i < files.length; i++) {
-			let file = files[i];
-			if(!file.endsWith(".js")) continue;
-
-			let model = require(path.join(dir, file));
-			model.knex(this._knex);
-			this.models[model.name] = model;
+		//Ensure that the connection is valid
+		try {
+			await this._knex.raw("SELECT 1 + 1");
+		} catch(e) {
+			console.error("Database connection error: " + e.message);
+			process.exit(1);
+			return;
 		}
+
+		//Load all of the models
+		this.models = await this._loadModels();
+	}
+
+	async _loadModels() {
+		let models = {};
+
+		let proxy = new ContainerProxy(this._app._container);
+
+		(await fs.readdir(this._options.modelPath))
+			.filter(f => f.endsWith("js")) //Filter to JavaScript files only
+			.map(f => path.join(this._options.modelPath, f)) //Map to the absolute path
+			.map(f => require(f)) //Require each model
+			.filter(fn => _.isFunction(fn)) //Filter out non-functions
+			.map(fn => (fn.prototype instanceof BaseModel) ? fn : fn(this._app, proxy)) //Transform the model class
+			.forEach(m => {
+				m.knex(this._knex); //Initialize knex
+				models[m.name] = m;
+			});
+
+		return models;
 	}
 };
