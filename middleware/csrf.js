2f06ddc833993c79a9503f98c0df7b6427c5c68b
Threebow <threebowpr@gmail.com>
2023-08-08 12:09:46 -0700

    fix: add csrf exclusion

--- middleware/csrf.js
+++ middleware/csrf.js
@@ -12,6 +12,14 @@ module.exports = class CSRFMiddleware extends Middleware {
 
 	async run(req, res) {
 		if(this._csurf) {
+			// Process exclusions
+			let exclusions = this._app._expressOptions.csrf.exclude;
+			if(exclusions) {
+				for(let i = 0; i < exclusions.length; i++) {
+					if(req.url.startsWith(exclusions[i])) return
+				}
+			}
+
 			await this._csurf(req, res);
 			res.locals.csrfToken = req.csrfToken();
 		}
