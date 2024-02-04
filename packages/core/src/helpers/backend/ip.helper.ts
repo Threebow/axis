86eb5e9a0034862507acfc164b14f3ab2b3e554b
Threebow <threebowpr@gmail.com>
2024-02-03 20:30:45 -0800

    fix: remove private IP restriction -- should ultimately be a middleware

--- packages/core/src/helpers/backend/ip.helper.ts
+++ packages/core/src/helpers/backend/ip.helper.ts
@@ -1,7 +1,6 @@
 import { IncomingMessage } from "http"
-import isPrivate from "private-ip"
 
-export function resolveIpAddressFromIncomingMessage(request: IncomingMessage, allowPrivate = false): string {
+export function resolveIpAddressFromIncomingMessage(request: IncomingMessage): string {
 	let ip = request.headers["do-connecting-ip"] ?? request.socket.remoteAddress
 	
 	if (Array.isArray(ip)) {
@@ -10,10 +9,6 @@ export function resolveIpAddressFromIncomingMessage(request: IncomingMessage, al
 	
 	if (!ip) {
 		throw new Error("IP address is inaccessible")
-	}
-	
-	if (!allowPrivate && isPrivate(ip)) {
-		throw new Error("Private IP addresses are prohibited.")
 	}
 	
 	return ip
