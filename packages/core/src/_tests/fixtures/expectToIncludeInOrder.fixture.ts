9b7e23dfbadd3619a8d3c2b17f0326124b9c7220
Threebow <threebowpr@gmail.com>
2024-03-16 14:54:36 -0700

    fix: fix expectToIncludeInOrder.fixture.ts

--- packages/core/src/_tests/fixtures/expectToIncludeInOrder.fixture.ts
+++ packages/core/src/_tests/fixtures/expectToIncludeInOrder.fixture.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai"
 
 export function expectToIncludeInOrder(val: any, targets: string[]) {
-	let lastIdx = 0
+	let lastIdx = -1
 	
 	expect(val).to.be.a("string")
 	
@@ -11,6 +11,7 @@ export function expectToIncludeInOrder(val: any, targets: string[]) {
 		const target = targets[i]
 		
 		const idx = str.indexOf(target)
+		
 		expect(idx).to.not.equal(-1, "Expected to find " + target + " in " + str)
 		expect(idx).to.be.greaterThan(lastIdx, "Expected " + target + " to not be out of order")
 		
