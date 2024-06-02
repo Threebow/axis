ce88e05154b43784ee9a507d1ee718bd369661c0
Threebow <threebowpr@gmail.com>
2024-06-02 09:56:04 -0700

    feat(request): attempt to parse json using built in parser

--- packages/core/src/helpers/axios.helper.ts
+++ packages/core/src/helpers/axios.helper.ts
@@ -1,21 +1,49 @@
 import axios, { AxiosError, AxiosRequestConfig, isAxiosError } from "axios"
 import { KVObject } from "../types"
 import { ErrorDTO } from "../dto"
+import { fromJson } from "./json.helper"
 
 export type RequestResultData = {
 	readonly [k: string]: any
 }
 
+// TODO: this entire file needs to be rethought
+
+/**
+ * Represents the result of an HTTP request.
+ */
 export type RequestResult<
 	T extends RequestResultData = any,
 	E extends ErrorDTO = ErrorDTO
 > = {
+	/**
+	 * Whether the request's status code is in the 2xx range.
+	 */
 	readonly success: true
+	
+	/**
+	 * The status code of the response.
+	 */
 	readonly status: number
+	
+	/**
+	 * The data returned by the server.
+	 */
 	readonly data: T
 } | {
+	/**
+	 * If the request returned non-2XX, this will be `false`.
+	 */
 	readonly success: false
+	
+	/**
+	 * The raw error thrown by Axios.
+	 */
 	readonly error: AxiosError
+	
+	/**
+	 * The standardized HTTP error DTO returned by the server. The status will be present in this object.
+	 */
 	readonly data: E
 }
 
@@ -28,16 +56,27 @@ export async function request<
 			url,
 			...opts,
 			headers: {
-				...opts.headers,
 				"Accept": "application/json",
-				"Content-Type": "application/json"
+				"Content-Type": "application/json",
+				...opts.headers
+			},
+			transformResponse(body, headers) {
+				if (headers.has("x-axis-json", "1")) {
+					return fromJson(body)
+				}
+				
+				if (headers.has("content-type", "application/json")) {
+					return JSON.parse(body)
+				}
+				
+				return body
 			}
 		})
 		
 		return {
 			success: true,
 			status: r.status,
-			data: r.data as T
+			data: r.data
 		}
 	} catch (e: any) {
 		if (isAxiosError(e) && e.response) {
@@ -50,11 +89,30 @@ export async function request<
 	}
 }
 
-export function createRequester(baseOpts?: AxiosRequestConfig) {
-	return <
+export interface IRequester {
+	/**
+	 * Makes a request. If this is a GET request and no query parameters are provided, the `data` parameter will be
+	 * used as for query parameters, as data is ignored for GET requests.
+	 */<
 		T extends RequestResultData = any,
 		E extends ErrorDTO = ErrorDTO
-	>(method: string, path: string, data?: KVObject, params?: KVObject, opts?: AxiosRequestConfig): Promise<RequestResult<T, E>> => {
+	>(
+		method: string,
+		path: string,
+		data?: KVObject,
+		params?: KVObject,
+		opts?: AxiosRequestConfig
+	): Promise<RequestResult<T, E>>
+}
+
+export function createRequester(baseOpts?: AxiosRequestConfig): IRequester {
+	function makeRequest<T extends RequestResultData = any, E extends ErrorDTO = ErrorDTO>(
+		method: string,
+		path: string,
+		data?: KVObject,
+		params?: KVObject,
+		opts?: AxiosRequestConfig
+	): Promise<RequestResult<T, E>> {
 		return request<T, E>(path, {
 			...baseOpts,
 			method,
@@ -63,4 +121,6 @@ export function createRequester(baseOpts?: AxiosRequestConfig) {
 			...opts
 		})
 	}
+	
+	return makeRequest
 }
