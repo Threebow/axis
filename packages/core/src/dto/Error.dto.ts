6556b9c3bed152947c700a663a4d57c900b6ff8c
Threebow <threebowpr@gmail.com>
2024-06-02 09:56:19 -0700

    docs: comment error dto

--- packages/core/src/dto/Error.dto.ts
+++ packages/core/src/dto/Error.dto.ts
@@ -1,5 +1,19 @@
+/**
+ * Represents an error that occurred during an HTTP request.
+ */
 export type ErrorDTO = {
+	/**
+	 * The status code of the response.
+	 */
 	status: number
+	
+	/**
+	 * An internal ID tracking the error message, usually populated by Sentry.
+	 */
 	eventId?: string
+	
+	/**
+	 * A human-readable message describing the error.
+	 */
 	extra?: string
 }
